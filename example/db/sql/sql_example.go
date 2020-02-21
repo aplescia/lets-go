1870f098d006a4c49bc8a826e38203b7552f71e1
Anthony Plescia <aplescia@chewy.com>
2020-02-21 11:07:51 -0500

    add examples

---
+++ example/db/sql/sql_example.go
@@ -0,0 +1,24 @@
+// +build example
+
+package main
+
+import (
+	"fmt"
+	"github.com/Chewy-Inc/lets-go/db/sql"
+	log "github.com/sirupsen/logrus"
+)
+
+var(
+	thing, _ = sql.OpenConnection("postgresql://localhost:5432/denver_replica?sslmode=disable")
+)
+
+func main() {
+	rows, err := thing.Raw("select * from promotion;").Rows()
+	if err != nil {
+		log.Error(err)
+	}
+	defer rows.Close()
+	for rows.Next(){
+		fmt.Println(rows)
+	}
+}
