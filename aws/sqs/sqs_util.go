9a1add800fdbb38d31b3584c0eae43efd62179cc
Anthony Plescia <aplescia@chewy.com>
2020-09-22 11:31:50 -0400

    initial commit

---
+++ aws/sqs/sqs_util.go
@@ -0,0 +1,30 @@
+package sqs
+
+import (
+	"github.com/aplescia-chwy/lets-go/util"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
+)
+
+var (
+	logger, _ = util.InitLoggerWithLevel(nil)
+)
+
+//BuildClient builds an SQS client using a passed AWS Config pointer.
+func BuildClient(config *aws.Config) *sqs.SQS {
+	lSession := session.Must(session.NewSession())
+
+	lSvc := sqs.New(lSession, config)
+
+	return lSvc
+}
+
+//PushToSqsAndReturnErrors pushes an input message to an SQS queue using a passed client object. Returns any errors.
+func PushToSqsAndReturnErrors(client *sqs.SQS, input *sqs.SendMessageInput) error {
+	_, err := client.SendMessage(input)
+	if err == nil {
+		logger.Printf("SQS Message Publish Successful To Queue %v! ", input.QueueUrl)
+	}
+	return err
+}
